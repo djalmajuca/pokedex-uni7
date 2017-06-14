0858c495a26d1d294d59d7aa3f494dd7e06a9077
Lucas Silva <lucas_s.s@hotmail.com>
2017-06-14 08:02:04 -0300

    Refinando mostrar mais informações

--- src/PokemonCard.js
+++ src/PokemonCard.js
@@ -25,8 +25,10 @@ class PokemonCard extends Component {
           <h1 className="color-strong-label">{this.props.pokemon.name}</h1>
           <span className="color-white">{this.props.pokemon.description}</span>
         </div>
+        <div style={{paddingBottom: '5px'}}>
           <button className="inf-button" onClick={this.showInfoCard}>Informações</button>
           <button className="dangerous-button" onClick={this.removeCard}>Deletar</button>
+        </div>
       </div>
     )
   }
